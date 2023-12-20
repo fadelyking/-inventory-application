f13a526351d48b6a020c2bbed7b0f6989d57de41
fadelyking <fadely195@gmail.com>
2023-12-20 14:08:53 +0300

    changes to db connection

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-
-const dev_db_url =
+mongoose.set("strictQuery", "false");
+const mongoDB =
 	"mongodb+srv://admin:admin@cluster0.uwhbfgl.mongodb.net/inventory-app?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
 	await mongoose.connect(mongoDB);
 }
+var app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+console.log(mongoose.connection.readyState);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
